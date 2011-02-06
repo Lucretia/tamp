6182220301864cf95ec2ab47c467d60a93b4e7ff
David Rees <dave@adakernel.org>
2011-02-07 07:49:50 +1300

    Added dynamic job scaling (Per CPU), and removed a redundant comment.

--- thirdparty/download.sh
+++ thirdparty/download.sh
@@ -226,9 +226,6 @@ fi
 #     echo "  >> Already have u-boot-$U_BOOT_VERSION.tar.bz2"
 # fi
 
-# TODO: Download and apply patches to other packages.
-# mpfr: patch -N -Z -p1 < allpatches
-
 cd $SRC
 
 #################################################################################
