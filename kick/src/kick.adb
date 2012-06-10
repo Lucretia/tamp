5546f4a077cd8f9d1833dcf1f7a51054f2b26729
Luke A. Guest <laguest@archeia.com>
2012-06-10 17:10:24 +0100

    More comments to document the process of "kicking" the machine.

--- kick/src/kick.adb
+++ kick/src/kick.adb
@@ -29,21 +29,29 @@ begin
    --
    --  GRUB on PC will have configured the machine to a certain point, similar
    --  with the binary blob on RPi. Kick will continue this initialisation so
-   --  that rest of the machine can be configured the way we need.
+   --  that rest of the machine can be configured the way we need. On 64-bit
+   --  PC machines, we need to go a step further to enable long mode.
    --
    --  Kick will set up the memory pages that are available and also those which
    --  are used (i.e. all the modules already in the system image will be
    --  "used").
    --
    --  Kick will assign these module's pages to a set of system processes, i.e.
-   --  Address Spaces, these will then be ready to run.
+   --  Address Spaces, these will then be ready to run. So, Kick needs to know
+   --  about TAMP's process structure.
    --
    --  TAMP needs to know which scheduler to use, so Kick must tell TAMP what
    --  that is, otherwise, when we jump to the kernel, the kernel won't be able
    --  to schedule any processes.
-   --    ** What is a scheduler? A simple procedure or a full process?
+   --    ** What is a scheduler? A simple procedure or a full process/service?
    --
-   --  
+   --  Kick will set the pages used by it's own program as being free so that
+   --  they can be reused by the kernel.
+   --
+   --  Kick will no jump into the TAMP kernel by calling it's initialise
+   --  subprogram. We will not return to Kick as TAMP will take over ending
+   --  by calling it's scheduler to start the processes we have loaded from
+   --  the system image.
 end Kick;
+pragma No_Return (Kick);
 
-   
